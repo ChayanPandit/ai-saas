af7817fb8d6718bd90d6131fcedbb2fef3d2e9ee
ChayanPandit <chayanpandit07@gmail.com>
2024-07-28 15:41:41 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Re:Image",
+  description: "AI-powered image recreation.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ 
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={{
+      variables: {colorPrimary: '#624cf5'}
+    }}>
+      <html lang="en">
+        <body className={inter.className}>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
